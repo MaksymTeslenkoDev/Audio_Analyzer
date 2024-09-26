6bc322c292b445a57bd1612e48478f4a042dd0b2
maksim.t <maksim.teslenko.dev@gmail.com>
2024-09-26 22:46:34 +0300

    add dev docker build, add error handler for validations errors

--- call_service/plugins/error-handler.js
+++ call_service/plugins/error-handler.js
@@ -3,6 +3,12 @@
 const fp = require('fastify-plugin');
 module.exports = fp(function (fastify, opts, next) {
   fastify.setErrorHandler((err, req, reply) => {
+    if (err.validation) {
+      const { validation, message } = err;
+      fastify.log.warn({ validationError: validation });
+      reply.status(422).send({ message });
+      return;
+    }
     if (reply.statusCode >= 500) {
       req.log.error({ req, res: reply, err: err }, err?.message);
       reply.send(`Fatal error. Contact the support team. Id ${req.id}`);
--- call_service/routes/routes.js
+++ call_service/routes/routes.js
@@ -1,17 +1,15 @@
 'use strict';
 
 module.exports = async function callServiceRoutes(fastify, opts) {
-  fastify.route(
-    {
-      method: 'POST',
-      url: '/call',
-      schema: {
-        body: fastify.getSchema('schema:call:add:body'),
-      },
-      handler: async function addCallHandler(request, reply) {
-        const { audio_url } = request.body;
-        return { message: 'Call service', audio_url };
-      },
+  fastify.route({
+    method: 'POST',
+    url: '/call',
+    schema: {
+      body: fastify.getSchema('schema:call:add:body'),
     },
-  );
+    handler: async function addCallHandler(request, reply) {
+      const { audio_url } = request.body;
+      return { message: 'Call service', audio_url };
+    },
+  });
 };
