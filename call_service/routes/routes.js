a344e881c9c4f9e1703a6d44ca42bb6a394a775c
maksim.t <maksim.teslenko.dev@gmail.com>
2024-10-03 12:41:21 +0300

    refactor function of getting audio transcription with vosk

--- call_service/routes/routes.js
+++ call_service/routes/routes.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const { getFileName } = require('../src/utils');
+const ffmpeg = require('fluent-ffmpeg');
 
+const sample_rate = 16000;
 module.exports = async function callServiceRoutes(fastify, opts) {
   fastify.route({
     method: 'POST',
@@ -13,17 +15,49 @@ module.exports = async function callServiceRoutes(fastify, opts) {
       const { audio_url } = request.body;
       fastify.log.info(`Call service with audio url: ${audio_url}`);
       const fileName = getFileName(audio_url);
-      const audio = await fetch(audio_url);
 
-      let transcription = null;
+      const rec = fastify.vosk.recognize(sample_rate);
+      rec.setMaxAlternatives(0);
+      rec.setWords(true);
+      rec.setPartialWords(true);
 
-      const buffer = []
+      const transcription = await transcribeAudio({
+        audio: audio_url,
+        recognizer: rec,
+        logger: fastify.log,
+      });
 
-      for await (let chunk of audio.body){
-        buffer.push(chunk);
-      }
-
-      return { message: 'Call service', audio_url, transcription };
+      return { fileName, transcription };
     },
   });
 };
+
+async function transcribeAudio({ audio, recognizer, logger }) {
+  const ffmpegProcess = ffmpeg()
+    .input(audio)
+    .inputFormat('wav')
+    .format('s16le')
+    .audioFrequency(sample_rate)
+    .audioChannels(1)
+    .audioCodec('pcm_s16le')
+    .on('error', (err) => {
+      logger.error(`FFmpeg error: ${err.message}`);
+      recognizer.free();
+      throw new Error(err.message);
+    });
+
+  try {
+    const ffmpegStream = ffmpegProcess.pipe();
+
+    for await (let chunk of ffmpegStream) {
+      recognizer.acceptWaveform(chunk);
+    }
+    const result = recognizer.finalResult();
+    recognizer.free();
+    const transcription = result.text;
+    return transcription;
+  } catch (err) {
+    recognizer.free();
+    throw err;
+  }
+}
